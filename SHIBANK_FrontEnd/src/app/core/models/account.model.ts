7d345de67df9db6d2a1be2d15356d48234000e86
Ivan Antiel Ilundayn <74334500+antiel29@users.noreply.github.com>
2024-05-08 11:37:31 -0300

    Delete SHIBANK_FrontEnd directory

--- SHIBANK_FrontEnd/src/app/core/models/account.model.ts
+++
@@ -1,5 +0,0 @@
-export class Account {
-    balance: number = 0.0;
-    id: number = 0;
-    accountNumber: string = '';
-}
--- SHIBANK_FrontEnd/src/app/core/models/transaction-create.model.ts
+++
@@ -1,6 +0,0 @@
-export class TransactionCreate {
-    amount: number = 0;
-    originAccountNumber: string = '';
-    destinyAccountNumber: string= '';
-    message: string= '';
-}
